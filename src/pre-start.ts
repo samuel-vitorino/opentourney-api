26ca4edf4f0941d73337d4e712a35844ef0bff70
Samuel Vitorino <samvitorino@gmail.com>
2023-04-26 16:18:06 +0100

    Minor fix

--- src/pre-start.ts
+++ src/pre-start.ts
@@ -29,9 +29,6 @@ const args = parse<IArgs>({
 });
 
 // Set the env file
-const result2 = dotenv.config({
+dotenv.config({
   path: path.join(__dirname, `../env/${args.env}.env`),
 });
-if (result2.error) {
-  throw result2.error;
-}
